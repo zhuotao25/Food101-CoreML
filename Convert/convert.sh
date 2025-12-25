35ce9c3543156deed28f65ad299649ccda69cd4d
michael.zt <7454681+zhuotao25@users.noreply.github.com>
2025-12-24 23:14:40 -0500

    Update convert.sh

--- Convert/convert.sh
+++ Convert/convert.sh
@@ -1,6 +1,32 @@
-wget https://s3.amazonaws.com/stratospark/food-101/model4b.10-0.68.hdf5
-virtualenv -p /usr/bin/python2.7 env
+#!/bin/bash
+set -e
+
+# Download model if it doesn't exist
+if [ ! -f "model4b.10-0.68.hdf5" ]; then
+    echo "Downloading model..."
+    curl -L -o model4b.10-0.68.hdf5 \
+        https://s3.amazonaws.com/stratospark/food-101/model4b.10-0.68.hdf5
+fi
+
+# Create virtual environment with Python 3
+if [ ! -d "env" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv env
+fi
+
+# Activate virtual environment
 source env/bin/activate
-pip install tensorflow keras==1.2.2 coremltools h5py
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install tensorflow keras h5py coremltools
+
+# Run conversion
+echo "Converting model to CoreML..."
 python food101.py
+
+# Deactivate
 deactivate
+
+echo "Conversion complete! Food101.mlmodel has been created."
